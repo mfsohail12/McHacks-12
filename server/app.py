63b0335012774652963b44c8a7bc25627f327949
Mona <monaw7777@gmail.com>
2025-01-25 18:46:25 -0500

    redesigned backend

--- server/app.py
+++ server/app.py
@@ -1,20 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template, request
+from patients import generate_mock_patient
+from flask_cors import CORS
 # from waitress import serve
 
 app = Flask(__name__)
+CORS(app)
 
-# @app.route('/')
-# @app.route('/index') 
-# def home():
-# 	return render_template('index.html')
 
-@app.route('/login', methods=['GET'])
-def login():
-	return render_template("login.html")
+num_of_patients = 10
 
-@app.route('/<user>')
-def user(user):
-	return f'<h1>{user}</h1>'
+@app.route('/dataset') # extra path for encryption
+def dataset():
+	sort = request.args.get('sort', 'arrival_time')
+	patients = [generate_mock_patient(num_of_patients) for _ in range(num_of_patients)]
+	patients.sort(key=lambda p: getattr(p, sort))
 
-app.debug = True
-app.run(host='0.0.0.0', port=8000)
+	return jsonify({
+		'Waiting Patients' : len(patients),
+		'patients': [p.serialize() for p in patients]
+	}) 
+
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run(host='0.0.0.0', port=8000)
