aef03beaf67f42f886abef481729e4fe9bcedf0d
Ritchie Yu <ritchieyu2002@gmail.com>
2025-01-25 16:35:44 -0500

    single player movement with boundaries

--- server/game.py
+++ server/game.py
@@ -1,27 +1,29 @@
 import pygame as pg
 from pygame.locals import KEYDOWN, QUIT
 
+# TODO: Put this on client side
 class Patient(pg.sprite.Sprite):
-    def __init__(self, color, width, height):
+    def __init__(self, color, width, height, xlim, ylim):
         super().__init__()
 
         self.image = pg.Surface([width, height])
         self.image.fill(color)
 
         self.rect = self.image.get_rect()  # get rectangle corresponding to image, position can be updated via rect.x and rect.y
+        self.xlim = xlim
+        self.ylim = ylim
 
-    def move_right(self, dist):
-        self.rect.x += dist
-    
-    def move_left(self, dist):
-        self.rect.x -= dist
-
-    def move_forward(self, dist):
-        self.rect.y += dist
-
-    def move_backward(self, dist):
-        self.rect.y -= dist
-
+    # TODO: Send updated player status to the game object
+    def update(self):
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT]:
+            self.rect.x = max(self.rect.x - 10, self.xlim[0])
+        if keys[pg.K_RIGHT]:
+            self.rect.x = min(self.rect.x + 10, self.xlim[1])
+        if keys[pg.K_UP]:
+            self.rect.y = max(self.rect.y - 10, self.ylim[0])
+        if keys[pg.K_DOWN]:
+            self.rect.y = min(self.rect.y + 10, self.ylim[1])
 
 class Game:
     def __init__(self, display_width, display_height):
@@ -29,8 +31,8 @@ class Game:
         self.dh = display_height
         self.patients = pg.sprite.Group()
 
-    def add_patient(self, color, width, height, position):
-        p = Patient(color, width, height)
+    def add_patient(self, color, width, height, xlim, ylim, position):
+        p = Patient(color, width, height, xlim, ylim)
         p.rect.x = position[0]
         p.rect.y = position[1]
         self.patients.add(p)
@@ -39,38 +41,34 @@ class Game:
         p.kill()
     
     def run(self):
+        '''
+        Implements the main game loop.
+        '''
         pg.init()
         screen = pg.display.set_mode((self.dw, self.dh))
         running = True
         clock = pg.time.Clock()
 
         while running:
+
             event = pg.event.poll()
             if event.type == QUIT:
                 running = False
             
-            keys = pg.key.get_pressed()
-            if keys[pg.K_LEFT]:
-                p0.move_left(10)
-            if keys[pg.K_RIGHT]:
-                p0.move_right(10)
-            if keys[pg.K_DOWN]:
-                p0.move_forward(10)
-            if keys[pg.K_UP]:
-                p0.move_backward(10)
-
-            p0.update()
+            self.patients.update()  # update state of each patient
             screen.fill((0, 128, 0))
-            all_patients.draw(screen)
-            pg.display.flip()
+            self.patients.draw(screen)  # draw patients to surface (arbitrary order)
+            pg.display.flip()  # update screen content
             clock.tick(60)
-
 
 
 DISPLAY_WIDTH = 800
 DISPLAY_HEIGHT = 800
+SPRITE_WIDTH = 30
+SPRITE_HEIGHT = 30
+XLIM = [0, DISPLAY_WIDTH-SPRITE_WIDTH]
+YLIM = [0, DISPLAY_HEIGHT-SPRITE_HEIGHT]
 
 game = Game(DISPLAY_WIDTH, DISPLAY_HEIGHT)
-
-
-
+game.add_patient((255,255,0), 30, 30, XLIM, YLIM, [0, 0])
+game.run()
