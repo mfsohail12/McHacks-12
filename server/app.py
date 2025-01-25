e8165cd61e557998cdba9649f8e308dc1a4182ca
mfsohail12 <149211499+mfsohail12@users.noreply.github.com>
2025-01-25 16:24:23 -0500

    frontend setup

--- server/app.py
+++ server/app.py
@@ -4,17 +4,20 @@ from waitress import serve
 app = Flask(__name__)
 
 @app.route('/')
+def h():
+	return render_template("login.html")
+
 @app.route('/index') 
 def home():
-	return render_template()
+	return render_template("index.html")
 
-@app.route('/login', methods=['POST', 'GET'])
+'''@app.route('/login', methods=['POST', 'GET'])
 def login():
-	return render_template("login.html")
+	return render_template("login.html")'''
 
 @app.route('/<user>')
 def user(user):
 	return f'<h1>{user}</h1>'
 
 if __name__ == '__main__':
-	serve(app, host='0.0.0.0', port=8000)
+	serve(app, host='0.0.0.0', port=5000)
