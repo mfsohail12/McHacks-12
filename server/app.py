e39f466b29879a1b92ad85c5ccf35f04139d117d
Mona <monaw7777@gmail.com>
2025-01-25 15:52:20 -0500

    init

---
+++ server/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from waitress import serve
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index') 
+def home():
+	return render_template()
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+	return render_template("login.html")
+
+@app.route('/<user>')
+def user(user):
+	return f'<h1>{user}</h1>'
+
+if __name__ == '__main__':
+	serve(app, host='0.0.0.0', port=8000)
