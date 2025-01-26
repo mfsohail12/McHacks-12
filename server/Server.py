7af434c5f0f63ff918281e03bbdb02dafb0e1553
Ritchie Yu <ritchieyu2002@gmail.com>
2025-01-26 09:19:07 -0500

    change what gets sent by server to client, from game object to dict

--- server/Server.py
+++ server/Server.py
@@ -1,92 +1,96 @@
 from _thread import *
 import sys
-from Player import Player
+from player import Patient
 import pickle
 import random
 import socket
+from constants import *
 
-server = "10.122.247.13"
+# server = "10.122.247.13"
+server = "localhost"
 port = 5555
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
-    s.bind((server, port))
+    server_socket.bind((server, port))
 except socket.error as e:
     print(str(e))
 
-s.listen()
+server_socket.listen()
 print("Waiting for a connection, Server Started")
 
 # List of players - dynamically growing as players connect
-players = []
-max_players = 5  # You want to support at least 5 players
+PATIENT_DATA = dict()
+MAX_PATIENTS = 5  # You want to support at least 5 players
 
-# Helper function to generate a random color
-def random_color():
-    return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-
-# Helper function to generate a random position near the center
-def random_position():
-    center_x = 250  # Center of the screen width
-    center_y = 250  # Center of the screen height
-    max_distance = 100  # Max distance from the center
-    x = random.randint(center_x - max_distance, center_x + max_distance)
-    y = random.randint(center_y - max_distance, center_y + max_distance)
-    return (x, y)
-
-def threaded_client(conn, player):
-    # Send the initial position of the current player
-    conn.send(pickle.dumps(players[player]))
-
+def threaded_client(conn, data):
+    # # Send the initial position of the current player
+    # conn.send(pickle.dumps(PATIENT_DATA[patient]))
+    patient = data['name']
     while True:
         try:
-            # Receive data from the client and update player
-            data = pickle.loads(conn.recv(2048))
-            players[player] = data
+            # # Receive data from the client and update player
+            # data = pickle.loads(conn.recv(2048))
+            PATIENT_DATA[patient] = data
 
             if not data:
-                print(f"Player {player} disconnected")
+                print(f"Player {patient} disconnected")
                 break
 
-            # Send the updated positions of all players to the current player
-            reply = [players[i] for i in range(len(players)) if i != player]
+            # Send the updated data of all players to the current player
+            reply = {k:v for k,v in PATIENT_DATA.items() if not(k == patient)}
 
-            print(f"Received from player {player}: {data}")
-            print(f"Sending to player {player}: {reply}")
+            # print(f"Received from player {patient}: {data}")
+            # print(f"Sending to player {patient}: {reply}")
 
             # Send the reply back to the client
+            import sys
+            print(sys.getsizeof(reply))
             conn.sendall(pickle.dumps(reply))
 
         except Exception as e:
-            print(f"Error in thread for player {player}: {e}")
+            print(f"Error in thread for player {patient}: {e}")
             break
 
-    print(f"Lost connection with player {player}")
+    print(f"Lost connection with player {patient}")
     conn.close()
 
 
-currentPlayer = 0
+current_patient = 0
 
 try:
     while True:
-        conn, addr = s.accept()
-        print("Connected to: ", addr)
+        client_socket, client_address = server_socket.accept()
+        # print("Connected to: ", client_address)
 
-        # Only allow up to max_players to connect
-        if currentPlayer < max_players:
-            # Create a new player with a random position and random color
-            x, y = random_position()
-            color = random_color()
-            players.append(Player(x, y, 50, 50, color))  # Starting position and random color for new player
-            start_new_thread(threaded_client, (conn, currentPlayer))
-            currentPlayer += 1
-        else:
-            print("Max players reached. Closing connection.")
-            conn.sendall(b"Server is full")
-            conn.close()
+        data = pickle.loads(client_socket.recv(1024))
+
+        # print("Received from client: ", data)
+
+        if current_patient < MAX_PATIENTS:
+            start_new_thread(threaded_client, (client_socket, data))
+
+        # # Only allow up to max_players to connect
+        # if currentPlayer < MAX_PATIENTS:
+        #     # Create a new player with a random position and random color
+        #     # x, y = random_position()
+        #     # color = random_color()
+
+        #     # p = Patient("Bob", PLAYER_WIDTH, PLAYER_HEIGHT, [0,0], XLIM, YLIM)
+        #     # p0_spritesheet = '/Users/Ritchie/Desktop/U4 Winter/mchacks/McHacks-12/server/graphics/16PixelSlime/BlueSlime/BlueSlimeWalking-Sheet.png'
+        #     # n_frames = 4
+        #     # p.init_sprite(p0_spritesheet, n_frames, SPRITE_RATE, PLAYER_WIDTH, PLAYER_HEIGHT, PLAYER_SCALE)
+        #     # PATIENT_DATA.append(p)
+
+        #     start_new_thread(threaded_client, (conn, currentPlayer))
+        #     currentPlayer += 1
+        # else:
+        #     print("Max players reached. Closing connection.")
+        #     conn.sendall(b"Server is full")
+        #     conn.close()
 
 except KeyboardInterrupt:
     print("Server interrupted by user")
 finally:
-    s.close()
+    server_socket.close()
