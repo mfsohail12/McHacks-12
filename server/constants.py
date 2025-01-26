7581b70d3b254f2c1240e4d57e1100923c016d5c
Ritchie Yu <ritchieyu2002@gmail.com>
2025-01-26 09:20:37 -0500

    initial commit

---
+++ server/constants.py
@@ -0,0 +1,16 @@
+DISPLAY_WIDTH  = 640
+DISPLAY_HEIGHT = 640
+
+PLAYER_WIDTH   = 16
+PLAYER_HEIGHT  = 16
+PLAYER_SCALE   = 4
+STAFF_WIDTH    = 40
+STAFF_HEIGHT   = 40
+SPRITE_RATE    = 20
+
+XLIM = [0, DISPLAY_WIDTH-PLAYER_WIDTH]
+YLIM = [0, DISPLAY_HEIGHT-PLAYER_HEIGHT]
+
+SPRITE_SHEETS = {'red':   '/Users/Ritchie/Desktop/U4 Winter/mchacks/McHacks-12/server/graphics/16PixelSlime/RedSlime/RedSlimeWalking-Sheet.png',
+                 'green': '/Users/Ritchie/Desktop/U4 Winter/mchacks/McHacks-12/server/graphics/16PixelSlime/GreenSlime/GreenSlimeWalking-Sheet.png',
+                 'blue':  '/Users/Ritchie/Desktop/U4 Winter/mchacks/McHacks-12/server/graphics/16PixelSlime/BlueSlime/BlueSlimeWalking-Sheet.png'}
