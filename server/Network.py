003f3c0119eda48a3ef53f7451fc55d51f07bbb0
Ritchie Yu <ritchieyu2002@gmail.com>
2025-01-26 09:18:06 -0500

    remove receive on connection

--- server/Network.py
+++ server/Network.py
@@ -5,27 +5,38 @@ import pickle
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "10.122.247.13"
+        # self.server = "10.122.247.13"
+        self.server = "localhost"
         self.port = 5555
         self.addr = (self.server, self.port)
-        self.p = self.connect()
+        self.connect()
+        # self.p = self.connect()
     
-    def getP(self):
-        return self.p
+    # def getP(self):
+    #     return self.p
 
     def connect(self):
         try:
             self.client.connect(self.addr)
-            return pickle.loads(self.client.recv(2048))
-
+            return
+            # return pickle.loads(self.client.recv(8048))
         except Exception as e:
             print(f"Connection error: {e}")
             return None
     
     def send(self, data):
         try:
-            self.client.send(pickle.dumps(data))
-            return pickle.loads(self.client.recv(2048))
-
+            import sys
+            print(sys.getsizeof(data))
+            self.client.sendall(pickle.dumps(data))
+            return pickle.loads(self.client.recv(20000))
         except socket.error as e:
             print(e)
+
+    # def receive(self):
+    #     try:
+    #         self.client.connect(self.addr)
+    #         return pickle.loads(self.client.recv(2048))
+    #     except Exception as e:
+    #         print(f"Connection error: {e}")
+    #         return None
