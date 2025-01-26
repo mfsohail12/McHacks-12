8243389527911b2e3495deca6b8137cff6e05ac7
mfsohail12 <149211499+mfsohail12@users.noreply.github.com>
2025-01-25 23:29:25 -0500

    updated flask frontend rediects to react frontend

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, render_template, request, redirect, url_for
 from patients import generate_mock_patient
 from flask_cors import CORS
+
 # from waitress import serve
 
 app = Flask(__name__)
@@ -9,39 +10,46 @@ CORS(app)
 
 num_of_patients = 10
 
-@app.route('/ifem/dataset') # extra path for encryption
+
+@app.route("/ifem/dataset")  # extra path for encryption
 def dataset():
-	sort = request.args.get('sort', 'arrival_time')
-	patients = [generate_mock_patient(num_of_patients) for _ in range(num_of_patients)]
-	patients.sort(key=lambda p: getattr(p, sort))
+    sort = request.args.get("sort", "arrival_time")
+    patients = [generate_mock_patient(num_of_patients) for _ in range(num_of_patients)]
+    patients.sort(key=lambda p: getattr(p, sort))
 
-	return jsonify({
-		'Waiting Patients' : len(patients),
-		'patients': [p.serialize() for p in patients]
-	})
+    return jsonify(
+        {
+            "Waiting Patients": len(patients),
+            "patients": [p.serialize() for p in patients],
+        }
+    )
 
 
-@app.route('/phases')
+@app.route("/phases")
 def phases():
-	return render_template('phases.html', title='Home')
+    return render_template("phases.html", title="Home")
 
 
-@app.route('/triage')
+@app.route("/triage")
 def triage():
-	return render_template('triage.html', title='Home')
+    return render_template("triage.html", title="Home")
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     error = None
-    if request.method == 'POST':
-        if request.form['username'] not in ['admin','user001','user002'] or request.form['password'] not in ['admin', 'user001', 'user002']:
-            error = 'Invalid Credentials. Please try again.'
+    if request.method == "POST":
+        if request.form["username"] not in [
+            "admin",
+            "user001",
+            "user002",
+        ] or request.form["password"] not in ["admin", "user001", "user002"]:
+            error = "Invalid Credentials. Please try again."
         else:
-            return redirect(url_for('phases'))
-    return render_template('login.html', error=error)
+            return redirect("http://localhost:5173/")
+    return render_template("login.html", error=error)
 
 
-if __name__ == '__main__':
-	app.debug = True
-	app.run(host='0.0.0.0', port=8000)
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host="0.0.0.0", port=8000)
